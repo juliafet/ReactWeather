10eefd2d9d09a8602e0b9d0ff645f5f55e96dbe9
Julia Fetisova <j.s.fetisova@gmail.com>
2021-08-03 13:49:06 +1000

    Initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+const Nav = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="" alt="" />
+                <h2 id="app-name">Weather</h2>
+            </div>
+        </nav>
+    )
+}
+
+import default Nav
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+const Search = () => {
+    return (
+        <div className="search">
+            <form action="">
+                <input type="text" id="search-bar" placeholder="Search cities" />
+                <button type="submit">Search</button> 
+            </form>
+        </div>
+    )
+
+}
+
+export default Search
